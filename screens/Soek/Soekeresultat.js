b26679461821ec62897a2fec61c635cde84e363e
Jeppe Jernberg <jeppe.jernberg@ciber.no>
2020-09-25 12:59:16 +0200

    Closes #9. La til bilde i søkeresultater

--- screens/Soek/Soekeresultat.js
+++ screens/Soek/Soekeresultat.js
@@ -1,73 +1,88 @@
-import React, { useEffect, useState } from "react";
-import { Text, View, StyleSheet, Image } from "react-native";
-import { api } from "../../service/api";
+import React from "react";
+import { Image, StyleSheet, Text, View } from "react-native";
 
 const Soekeresultat = props => {
   const { produkt, navigation } = props;
-  const [produktBilde, setProduktBilde] = useState(null);
-
-  useEffect(() => {
-    api
-      .get(
-        `https://bilder.vinmonopolet.no/cache/300x300-0/${produkt.basic.productId}-1.jpg`
-      )
-      .then(bilde => {
-        setProduktBilde(bilde);
-      });
-  }, []);
-
   return (
-    <View style={styles.produktKontainer}>
-      <Image source={produktBilde} />
-      <Text style={styles.produktType}>
-        {produkt.classification.productTypeName.toUpperCase()}
-      </Text>
-      <Text style={styles.produktNavn}>
-        {produkt.basic.productShortName} {produkt.basic.vintage}
-      </Text>
-      <Text style={styles.produktRegion}>
-        {produkt.origins.origin.country}
-        {produkt.origins.origin.region && `, ${produkt.origins.origin.region}`}
-        {produkt.origins.origin.subRegion &&
-          `, ${produkt.origins.origin.subRegion}`}
-      </Text>
-      <Text style={styles.produktId}>{produkt.basic.productId}</Text>
-      <Text style={styles.produktPris}>
-        Kr. {Number.parseFloat(produkt.prices[0].salesPrice).toFixed(2)}
-      </Text>
+    <View style={styles.produktContainer}>
+      <View style={styles.bildeContainer}>
+        <Image
+          style={styles.bilde}
+          source={{
+            uri: `https://bilder.vinmonopolet.no/cache/515x515-0/${produkt.basic.productId}-1.jpg`
+          }}
+        />
+      </View>
+      <View style={styles.infoContainer}>
+        <Text style={styles.produktType}>
+          {produkt.classification.productTypeName.toUpperCase()}
+        </Text>
+        <Text style={styles.produktNavn}>
+          {produkt.basic.productShortName} {produkt.basic.vintage}
+        </Text>
+        <Text style={styles.produktRegion}>
+          {produkt.origins.origin.country}
+          {produkt.origins.origin.region &&
+            `, ${produkt.origins.origin.region}`}
+          {produkt.origins.origin.subRegion &&
+            `, ${produkt.origins.origin.subRegion}`}
+        </Text>
+        <Text style={styles.produktId}>{produkt.basic.productId}</Text>
+        <Text style={styles.produktPris}>
+          Kr. {Number.parseFloat(produkt.prices[0].salesPrice).toFixed(2)}
+        </Text>
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  produktKontainer: {
+  produktContainer: {
     flex: 1,
+    flexDirection: "row",
     padding: 15,
-    borderBottomWidth: 0.5
+    borderBottomWidth: 0.5,
+    height: 250
+  },
+  bildeContainer: {
+    width: "20%",
+    marginRight: 15
+  },
+  infoContainer: {
+    width: "80%"
   },
   produktNavn: {
     fontSize: 20,
-    fontWeight: "bold"
+    fontWeight: "bold",
+    paddingBottom: 10,
   },
   produktId: {
     fontSize: 15,
-    color: "gray"
+    color: "gray",
+    paddingBottom: 10,
   },
   produktRegion: {
-    fontSize: 17
+    fontSize: 17,
+    paddingBottom: 10,
   },
   produktPris: {
     fontSize: 17,
-    fontWeight: "bold"
+    fontWeight: "bold",
+    paddingBottom: 10,
   },
   produktType: {
     fontSize: 15,
-    color: "gray"
+    color: "gray",
+    paddingBottom: 10,
   },
   item: {
     padding: 20,
     marginVertical: 8,
     marginHorizontal: 16
+  },
+  bilde: {
+    height: "100%",
+    width: "100%"
   }
 });
 
--- styles/common.js
+++ styles/common.js
@@ -1,5 +1,20 @@
 export const colors = {
   primaryBg: "#282c34",
   primarySelected: "#fff",
-  borderColor: "#3a5d57",
+  borderColor: "#3a5d57"
+};
+
+export const text = {
+  h1: {
+    fontSize: "",
+    paddingBottom: 10
+  },
+  h2: {
+    fontSize: "",
+    paddingBottom: 8
+  },
+  h3: {
+    fontSize: "",
+    paddingBottom: 5
+  },
 };
