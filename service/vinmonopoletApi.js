3042d150182279f07cae61fe87a1c0e077c92012
Jeppe Jernberg <jeppe.jernberg@ciber.no>
2021-05-11 14:21:08 +0200

    #48: Tok i bruk nytt API for søk

--- service/vinmonopoletApi.js
+++ service/vinmonopoletApi.js
@@ -1,8 +1,11 @@
 export const vinmonopolet_config = {
   headers: {
-    "Ocp-Apim-Subscription-Key": "b2c1c8d6dfa9405db0ff0c3411ed1ed2"
+    "Ocp-Apim-Subscription-Key": "1ad2d3b366114954a2136386dcd79352"
   }
 };
 
 export const produktIdSoek = produktId =>
-  `https://apis.vinmonopolet.no/products/v0/details-normal?productId=${produktId}`;
+  `https://apis.vinmonopolet.no/press-products/v1/details-normal?productId=${produktId}`;
+
+export const fritekstSoek = fritekst =>
+  `https://apis.vinmonopolet.no/press-products/v1/details-normal?freeText=${fritekst}`;
